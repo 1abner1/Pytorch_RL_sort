cace3c42f43e698515b16a5955a6543d802a1cd2
abner <axiaowwa@163.com>
2021-11-09 20:17:58 +0800

    first

---
+++ algorithm/sac_zy/Advanced-Soft-Actor-Critic-master/models/3DBall/20210611100147nJCl/nn_models.py
@@ -0,0 +1,19 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+import algorithm.nn_models as m
+
+ModelRep = m.ModelSimpleRep
+
+
+class ModelQ(m.ModelQ):
+    def __init__(self, state_dim, d_action_dim, c_action_dim, name=None):
+        super().__init__(state_dim, d_action_dim, c_action_dim,
+                         dense_n=64, dense_depth=2)
+
+
+class ModelPolicy(m.ModelPolicy):
+    def __init__(self, state_dim, d_action_dim, c_action_dim, name=None):
+        super().__init__(state_dim, d_action_dim, c_action_dim,
+                         dense_n=64, dense_depth=2)
